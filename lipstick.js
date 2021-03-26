49e812aeb7f258c610cf0d80087779ad7b69bcda
niveditha10 <73229406+niveditha10@users.noreply.github.com>
2021-03-26 12:40:11 +0530

    Add files via upload

---
+++ lipstick.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(300,300);
+canvas.position(500,250);
+video=createCapture();
+video.hide();
+
+}
+
+function draw(){
+    image(video,0,0,300,300);
+  }
+ 
+function take_snapshot(){
+    save("myfilterimage.png");
+}
